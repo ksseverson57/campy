2b35ac4d03ea0cc5c351d362e091feac4fec9ccf
Kyle Severson <ksevers2@jhmi.edu>
2020-08-20 16:35:33 -0400

    display update, bug fixes
    Accounts for lack of PylonImageWindow on linux systems. However, matplotlib pyplot is less performant.

--- display/display.py
+++ display/display.py
@@ -1,39 +1,72 @@
 """
 
 """
-
+import sys
 import time
+import logging
 import numpy as np
+
 import matplotlib as mpl
 mpl.use('Qt5Agg') # disregard qtapp warning...
 import matplotlib.pyplot as plt
 
+import pypylon.pylon as pylon
+import pypylon.genicam as geni
+
 def draw_figure(num):
-    mpl.rcParams['toolbar'] = 'None' 
+	mpl.rcParams['toolbar'] = 'None' 
 
-    figure = plt.figure(num)
-    ax = plt.axes([0,0,1,1], frameon=False)
+	figure = plt.figure(num)
+	ax = plt.axes([0,0,1,1], frameon=False)
 
-    plt.axis('off')
-    plt.autoscale(tight=True)
-    plt.ion()
+	plt.axis('off')
+	plt.autoscale(tight=True)
+	plt.ion()
 
-    imageWindow = ax.imshow(np.zeros((1,1), dtype='uint8'), 
-        interpolation='none')
+	imageWindow = ax.imshow(np.zeros((1,1,3), dtype='uint8'), 
+		interpolation='none')
 
-    figure.canvas.draw()
-    plt.show(block=False)
+	figure.canvas.draw()
+	plt.show(block=False)
 
-    return figure, imageWindow
+	return figure, imageWindow
 
-def DisplayFrames(c, dispQueue):
-    figure, imageWindow = draw_figure(c+1)
-    while(True):
-        if dispQueue:
-            # dispQueue.popleft()
-            imageWindow.set_data(dispQueue.popleft())
-            figure.canvas.draw()
-            figure.canvas.flush_events()
-        else:
-            time.sleep(0.01)
-    plt.close(figure)
+def DisplayFrames(c, dispQueue, ds, meta):
+	if sys.platform == "win32" and meta['CameraMake'] == 'basler':
+		imageWindow = pylon.PylonImageWindow()
+		imageWindow.Create(c)
+		imageWindow.Show()
+		while(True):
+			try:
+				if dispQueue:
+					grabResult = dispQueue.popleft()
+					try:
+						imageWindow.SetImage(grabResult)
+						imageWindow.Show()
+						grabResult.Release()
+					except Exception as e:
+						logging.error('Caught exception: {}'.format(e))
+					except:
+						grabResult.Release()
+				else:
+					time.sleep(0.01)
+			except KeyboardInterrupt:
+				break
+		imageWindow.Close()
+	else:
+		figure, imageWindow = draw_figure(c+1)
+		while(True):
+			try:
+				if dispQueue:
+					img = dispQueue.popleft()
+					try:
+						imageWindow.set_data(img)
+						figure.canvas.draw()
+						figure.canvas.flush_events()
+					except Exception as e:
+						logging.error('Caught exception: {}'.format(e))
+				else:
+					time.sleep(0.01)
+			except KeyboardInterrupt:
+				break
+		plt.close(figure)
