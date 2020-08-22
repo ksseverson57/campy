669792cb578942c8ffbc83b826e7ec8022521264
diegoaldarondo@g.harvard.edu <diegoaldarondo@g.harvard.edu>
2020-08-22 18:38:09 -0400

    Point entrypoint to campy.campy:main.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
 					],
     entry_points={
         "console_scripts": [
-            "campy-acquire = campy.acquisition:main"
+            "campy-acquire = campy.campy:main"
         ]
     }
 )
