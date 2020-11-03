4e923cb59e5fe18caf1cbeb3e28781bb32f0247b
Kyle Severson <ksevers2@jhmi.edu>
2020-11-03 18:20:43 -0500

    updated x264 parameters for CPU compression

--- campy/writer/campipe.py
+++ campy/writer/campipe.py
@@ -1,55 +1,108 @@
 """
 
 """
+
 from imageio_ffmpeg import write_frames
 import os
 import time
 import logging
 import sys
 
-def OpenWriter(
-		cam_params,
-		fname='0',
-		ext='.mp4',
-		codec='h264_nvenc',
-		loglevel='quiet', # 'warning', 'quiet', 'info'
-		):
-	
+def OpenWriter(cam_params):
 	n_cam = cam_params["n_cam"]
 
-	folder_name = os.path.join(cam_params["videoFolder"], cam_params["cameraName"])
-	file_name = os.path.join(folder_name, fname + ext)
+	folder_name = cam_params["videoFolder"]
+	if len(cam_params["cameraNames"]) > 1:
+		folder_name = os.path.join(folder_name, cam_params["cameraName"])
+	file_name = cam_params["videoFilename"]
+	full_file_name = os.path.join(folder_name, file_name)
 
 	if not os.path.isdir(folder_name):
 		os.makedirs(folder_name)
 		print('Made directory {}.'.format(folder_name))
 	
+	# Load defaults
+	pix_fmt_out = cam_params["pixelFormatOutput"]
+	codec = cam_params["codec"]
+	gpu_params = []
+
+	# CPU compression
+	if cam_params["gpuID"] == -1:
+		print('Opened: {} using CPU to compress the stream.'.format(full_file_name))
+		if pix_fmt_out == 'rgb0':
+			pix_fmt_out = 'yuv420p'
+		if cam_params["codec"] == 'h264':
+			codec = 'libx264'
+		elif cam_params["codec"] == 'h265':
+			codec = 'libx265'
+		gpu_params = ['-r:v', str(cam_params["frameRate"]),
+					'-preset', 'fast',
+					'-tune', 'fastdecode',
+					'-crf', cam_params["quality"],
+					'-bufsize', '20M',
+					'-maxrate', '10M',
+					'-bf:v', '4',
+					'-vsync', '0',]
+
+	# GPU compression
+	else:
+		print('Opened: {} using GPU {} to compress the stream.'.format(full_file_name, cam_params["gpuID"]))
+		if cam_params["gpuMake"] == 'nvidia':
+			if cam_params["codec"] == 'h264':
+				codec = 'h264_nvenc'
+			elif cam_params["codec"] == 'h265':
+				codec = 'hevc_nvenc'
+			gpu_params = ['-r:v', str(cam_params["frameRate"]), # important to play nice with vsync '0'
+						'-preset', 'fast', # set to 'fast', 'llhp', or 'llhq' for h264 or hevc
+						'-qp', cam_params["quality"],
+						'-bf:v', '0',
+						'-vsync', '0',
+						'-2pass', '0',
+						'-gpu', str(cam_params["gpuID"]),]
+		elif cam_params["gpuMake"] == 'amd':
+			if pix_fmt_out == 'rgb0':
+				pix_fmt_out = 'yuv420p'
+			if cam_params["codec"] == 'h264':
+				codec = 'h264_amf'
+			elif cam_params["codec"] == 'h265':
+				codec = 'hevc_amf'
+			gpu_params = ['-r:v', str(cam_params["frameRate"]),
+						'-usage', 'lowlatency',
+						'-rc', 'cqp', # constant quantization parameter
+						'-qp_i', cam_params["quality"],
+						'-qp_p', cam_params["quality"],
+						'-qp_b', cam_params["quality"],
+						'-bf:v', '0',
+						'-hwaccel', 'auto',
+						'-hwaccel_device', str(cam_params["gpuID"]),]
+		elif cam_params["gpuMake"] == 'intel':
+			if pix_fmt_out == 'rgb0':
+				pix_fmt_out = 'nv12'
+			if cam_params["codec"] == 'h264':
+				codec = 'h264_qsv'
+			elif cam_params["codec"] == 'h265':
+				codec = 'hevc_qsv'
+			gpu_params = ['-r:v', str(cam_params["frameRate"]),
+						'-bf:v', '0',]
+
+	# Initialize writer object (imageio-ffmpeg)
 	while(True):
 		try:
 			try:
 				writer = write_frames(
-							file_name,
-							[cam_params["frameWidth"], cam_params["frameHeight"]], # size [W,H]
-							fps=cam_params["frameRate"],
-							quality=None,
-							codec=codec,  # H.265 hardware accel'd (GPU) 'hevc_nvenc'; H.264 'h264_nvenc'
-							pix_fmt_in=cam_params["pixelFormatInput"], # 'bayer_bggr8', 'gray', 'rgb24', 'bgr0', 'yuv420p'
-							pix_fmt_out=cam_params["pixelFormatOutput"], # 'rgb0' (fastest), 'yuv420p'(slower), 'bgr0' (slower)
-							bitrate=None,
-							ffmpeg_log_level=loglevel, # 'warning', 'quiet', 'info'
-							input_params=['-an'], # '-an' no audio
-							output_params=[
-								'-preset', 'fast', # set to 'fast', 'llhp', or 'llhq' for h264 or hevc
-								'-qp', cam_params["quality"],
-								'-r:v', str(cam_params["frameRate"]), # important to play nice with vsync '0'
-								'-bf:v', '0',
-								'-vsync', '0',
-								'-2pass', '0',
-								'-gpu', str(cam_params["gpu"]),
-								],
-							)
+					full_file_name,
+					[cam_params["frameWidth"], cam_params["frameHeight"]], # size [W,H]
+					fps=cam_params["frameRate"],
+					quality=None,
+					codec=codec,
+					pix_fmt_in=cam_params["pixelFormatInput"], # 'bayer_bggr8', 'gray', 'rgb24', 'bgr0', 'yuv420p'
+					pix_fmt_out=pix_fmt_out,
+					bitrate=None,
+					ffmpeg_log_level=cam_params["ffmpegLogLevel"], # 'warning', 'quiet', 'info'
+					input_params=['-an'], # '-an' no audio
+					output_params=gpu_params,
+					)
 				writer.send(None) # Initialize the generator
-				print('Opened: {} using GPU {}.'.format(file_name, cam_params["gpu"]))
 				break
 			except Exception as e:
 				logging.error('Caught exception: {}'.format(e))
@@ -61,7 +114,6 @@ def OpenWriter(
 	return writer
 
 def WriteFrames(cam_params, writeQueue, stopQueue):
-
 	n_cam = cam_params["n_cam"]
 
 	# Start ffmpeg video writer 
