d79ccf81a9d62bf471ba884361fedb3b90c63ca1
Kyle Severson <ksevers2@jhmi.edu>
2021-03-02 14:14:16 -0500

    updated campy version to 1.0.1
    campy entering beta testing

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
 	name='campy',
-	version='0.2.0',
-    packages=find_packages(),
+	version='1.0.0',
+	packages=find_packages(),
 	install_requires=[
 					'numpy',
 					'imageio',
 					'imageio-ffmpeg',
 					'scikit-image',
-                    'pyyaml',
+					'pyyaml',
 					],
-    entry_points={
-        "console_scripts": [
-            "campy-acquire = campy.campy:Main"
-        ]
-    }
+	entry_points={
+		"console_scripts": [
+			"campy-acquire = campy.campy:Main"
+		]
+	}
 )
