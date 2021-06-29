f826676b9f95394b0a31fd38d986db3eea95116f
Kyle Severson <ksevers2@jhmi.edu>
2021-06-29 18:44:10 -0400

    fixed exceptions

--- campy/writer/campipe.py
+++ campy/writer/campipe.py
@@ -1,7 +1,5 @@
 """
-
 """
-
 from imageio_ffmpeg import write_frames
 import os
 import time
@@ -9,12 +7,10 @@ import logging
 import sys
 
 def OpenWriter(cam_params):
-	n_cam = cam_params["n_cam"]
-
+	writing = False
 	folder_name = os.path.join(cam_params["videoFolder"], cam_params["cameraName"])
-	if cam_params["cameraMake"] == "emu":
-		cam_params["videoFilename"] = "emu" + cam_params["videoFilename"]
-	full_file_name = os.path.join(folder_name, cam_params["videoFilename"])
+	file_name = cam_params["videoFilename"]
+	full_file_name = os.path.join(folder_name, file_name)
 
 	if not os.path.isdir(folder_name):
 		os.makedirs(folder_name)
@@ -23,7 +19,11 @@ def OpenWriter(cam_params):
 	# Load defaults
 	pix_fmt_out = cam_params["pixelFormatOutput"]
 	codec = cam_params["codec"]
+	quality = str(cam_params["quality"])
+	frameRate = str(cam_params["frameRate"])
+	gpuID = str(cam_params["gpuID"])
 	gpu_params = []
+	
 
 	# CPU compression
 	if cam_params["gpuID"] == -1:
@@ -34,10 +34,10 @@ def OpenWriter(cam_params):
 			codec = 'libx264'
 		elif cam_params["codec"] == 'h265':
 			codec = 'libx265'
-		gpu_params = ['-r:v', str(cam_params["frameRate"]),
+		gpu_params = ['-r:v', frameRate,
 					'-preset', 'fast',
 					'-tune', 'fastdecode',
-					'-crf', cam_params["quality"],
+					'-crf', quality,
 					'-bufsize', '20M',
 					'-maxrate', '10M',
 					'-bf:v', '4',
@@ -51,13 +51,13 @@ def OpenWriter(cam_params):
 				codec = 'h264_nvenc'
 			elif cam_params["codec"] == 'h265':
 				codec = 'hevc_nvenc'
-			gpu_params = ['-r:v', str(cam_params["frameRate"]), # important to play nice with vsync '0'
+			gpu_params = ['-r:v', frameRate, # important to play nice with vsync '0'
 						'-preset', 'fast', # set to 'fast', 'llhp', or 'llhq' for h264 or hevc
-						'-qp', cam_params["quality"],
+						'-qp', quality,
 						'-bf:v', '0',
 						'-vsync', '0',
 						'-2pass', '0',
-						'-gpu', str(cam_params["gpuID"]),]
+						'-gpu', gpuID,]
 		elif cam_params["gpuMake"] == 'amd':
 			if pix_fmt_out == 'rgb0':
 				pix_fmt_out = 'yuv420p'
@@ -65,15 +65,15 @@ def OpenWriter(cam_params):
 				codec = 'h264_amf'
 			elif cam_params["codec"] == 'h265':
 				codec = 'hevc_amf'
-			gpu_params = ['-r:v', str(cam_params["frameRate"]),
+			gpu_params = ['-r:v', frameRate,
 						'-usage', 'lowlatency',
 						'-rc', 'cqp', # constant quantization parameter
-						'-qp_i', cam_params["quality"],
-						'-qp_p', cam_params["quality"],
-						'-qp_b', cam_params["quality"],
+						'-qp_i', quality,
+						'-qp_p', quality,
+						'-qp_b', quality,
 						'-bf:v', '0',
 						'-hwaccel', 'auto',
-						'-hwaccel_device', str(cam_params["gpuID"]),]
+						'-hwaccel_device', gpuID,]
 		elif cam_params["gpuMake"] == 'intel':
 			if pix_fmt_out == 'rgb0':
 				pix_fmt_out = 'nv12'
@@ -81,59 +81,58 @@ def OpenWriter(cam_params):
 				codec = 'h264_qsv'
 			elif cam_params["codec"] == 'h265':
 				codec = 'hevc_qsv'
-			gpu_params = ['-r:v', str(cam_params["frameRate"]),
+			gpu_params = ['-r:v', frameRate,
 						'-bf:v', '0',]
 
 	# Initialize writer object (imageio-ffmpeg)
 	while(True):
 		try:
-			try:
-				writer = write_frames(
-					full_file_name,
-					[cam_params["frameWidth"], cam_params["frameHeight"]], # size [W,H]
-					fps=cam_params["frameRate"],
-					quality=None,
-					codec=codec,
-					pix_fmt_in=cam_params["pixelFormatInput"], # 'bayer_bggr8', 'gray', 'rgb24', 'bgr0', 'yuv420p'
-					pix_fmt_out=pix_fmt_out,
-					bitrate=None,
-					ffmpeg_log_level=cam_params["ffmpegLogLevel"], # 'warning', 'quiet', 'info'
-					input_params=['-an'], # '-an' no audio
-					output_params=gpu_params,
-					)
-				writer.send(None) # Initialize the generator
-				break
-			except Exception as e:
-				logging.error('Caught exception: {}'.format(e))
-				time.sleep(0.1)
+			writer = write_frames(
+				full_file_name,
+				[cam_params["frameWidth"], cam_params["frameHeight"]], # size [W,H]
+				fps=cam_params["frameRate"],
+				quality=None,
+				codec=codec,
+				pix_fmt_in=cam_params["pixelFormatInput"], # 'bayer_bggr8', 'gray', 'rgb24', 'bgr0', 'yuv420p'
+				pix_fmt_out=pix_fmt_out,
+				bitrate=None,
+				ffmpeg_log_level=cam_params["ffmpegLogLevel"], # 'warning', 'quiet', 'info'
+				input_params=['-an'], # '-an' no audio
+				output_params=gpu_params,
+				)
+			writer.send(None) # Initialize the generator
+			writing = True
+			break
 
 		except KeyboardInterrupt:
 			break
+			
+		except Exception as e:
+			logging.error('Caught exception: {}'.format(e))
+			time.sleep(0.1)
 
-	return writer
+	return writer, writing
 
 def WriteFrames(cam_params, writeQueue, stopQueue):
-	n_cam = cam_params["n_cam"]
-
 	# Start ffmpeg video writer 
-	writer = OpenWriter(cam_params)
+	writer, writing = OpenWriter(cam_params)
 	message = ''
 
-	# Continue writing...
-	while(True):
+	# Write until interrupted or stop message received
+	while(writing):
 		try:
 			if writeQueue:
 				message = writeQueue.popleft()
-				if not isinstance(message, str):
+				if isinstance(message, str) and message=='STOP':
+					writing = False
+				else:
 					writer.send(message)
-				elif message=='STOP':
-					break
 			else:
-				time.sleep(0.0001)
+				time.sleep(0.001)
 		except KeyboardInterrupt:
-			stopQueue.append('STOP GRABBING')
+			stopQueue.append('STOP')
 
-	# Closing up...
-	print('Closing video writer for camera {}. Please wait...'.format(n_cam+1))
-	time.sleep(1)
+	# Close up...
+	print('Closing video writer for {}. Please wait...'.format(cam_params["cameraName"]))
 	writer.close()
+	time.sleep(2)
