51750e1bb33744ce2c9ab3221f9f9250803d6ff5
Kyle Severson <ksevers2@jhmi.edu>
2021-06-29 18:37:30 -0400

    updated setup for arduino serial trigger

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
 	name='campy',
-	version='0.2.0',
-    packages=find_packages(),
+	version='1.0.0',
+	packages=find_packages(),
 	install_requires=[
 					'numpy',
 					'imageio',
 					'imageio-ffmpeg',
 					'scikit-image',
-                    'pyyaml',
+					'matplotlib',
+					'pyyaml',
+					'pyserial',
 					],
-    entry_points={
-        "console_scripts": [
-            "campy-acquire = campy.campy:Main"
-        ]
-    }
+	entry_points={
+		"console_scripts": [
+			"campy-acquire = campy.campy:Main"
+		]
+	}
 )
