10cf36bc3328eef692b210a89a94dc6faa5b26d1
Kyle Severson <ksevers2@jhmi.edu>
2021-06-29 18:42:12 -0400

    Add files via upload

--- campy/cameras/emu/cam.py
+++ campy/cameras/emu/cam.py
@@ -2,6 +2,7 @@
 
 """
 
+from campy.cameras import unicam
 import os
 import time
 import logging
@@ -11,13 +12,26 @@ from collections import deque
 import csv
 import imageio
 
-def OpenCamera(cam_params, bufferSize=500, validation=False):
-	n_cam = cam_params["n_cam"]
-	cam_index = cam_params["cameraSelection"]
-	camera_name = cam_params["cameraName"]
+def LoadSystem(params):
 
+	return params["cameraMake"]
+
+def GetDeviceList(system):
+
+	return system
+
+def LoadDevice(cam_params):
+
+	return cam_params["device"]
+
+def GetSerialNumber(device):
+
+	return device
+
+def OpenCamera(cam_params, device):
 	# Open video reader for emulation
-	full_file_name = os.path.join(cam_params["videoFolder"],cam_params["cameraName"],cam_params["videoFilename"])
+	videoFileName = cam_params["videoFilename"][3:len(cam_params["videoFilename"])]
+	full_file_name = os.path.join(cam_params["videoFolder"], cam_params["cameraName"], videoFileName)
 	camera = imageio.get_reader(full_file_name)
 
 	# Set features manually or automatically, depending on configuration
@@ -25,114 +39,45 @@ def OpenCamera(cam_params, bufferSize=500, validation=False):
 	cam_params['frameWidth'] = frame_size[0]
 	cam_params['frameHeight'] = frame_size[1]
 
-	# Start grabbing frames (OneByOne = first in, first out)
-	camera.MaxNumBuffer = bufferSize
-	print("Started", camera_name, "emulation.")
+	print("Opened {} emulation.".format(cam_params["cameraName"]))
 	return camera, cam_params
 
-def GrabFrames(cam_params, camera, writeQueue, dispQueue, stopQueue):
-	n_cam = cam_params["n_cam"]
+def LoadSettings(cam_params, camera):
 
-	# Create dictionary for appending frame number and timestamp information
-	grabdata = {}
-	grabdata['timeStamp'] = []
-	grabdata['frameNumber'] = []
+	return cam_params
 
-	numImagesToGrab = cam_params['recTimeInSec']*cam_params['frameRate']
-	chunkLengthInFrames = int(round(cam_params["chunkLengthInSec"]*cam_params['frameRate']))
+def StartGrabbing(camera):
 
-	if cam_params["displayFrameRate"] <= 0:
-		frameRatio = float('inf')
-	elif cam_params["displayFrameRate"] > 0 and cam_params["displayFrameRate"] <= cam_params['frameRate']:
-		frameRatio = int(round(cam_params['frameRate']/cam_params["displayFrameRate"]))
-	else:
-		frameRatio = cam_params['frameRate']
-	print(cam_params["cameraName"], "ready to emulate.")
+	return True
 
-	cnt = 0
-	while(True):
-		if stopQueue or cnt >= numImagesToGrab:
-			CloseCamera(cam_params, camera, grabdata)
-			writeQueue.append('STOP')
-			break
-		try:
-			timeStart = time.perf_counter()
-			# Grab image from camera buffer if available
-			grabResult = camera.get_data(cnt)
+def GrabFrame(camera, frameNumber):
 
-			# Append numpy array to writeQueue for writer to append to file
-			writeQueue.append(grabResult)
+	return camera.get_data(frameNumber)
 
-			if cnt == 0:
-				timeFirstGrab = time.perf_counter()
-			grabtime = (time.perf_counter() - timeFirstGrab)
-			grabdata['timeStamp'].append(grabtime)
+def GetImageArray(grabResult, cam_params):
 
-			cnt += 1
-			grabdata['frameNumber'].append(cnt) # first frame = 1
+	return grabResult
 
-			if cnt % frameRatio == 0:
-				dispQueue.append(grabResult[::cam_params["displayDownsample"],
-											::cam_params["displayDownsample"],
-											:])
-			if cnt % chunkLengthInFrames == 0:
-				fps_count = int(round(cnt/grabtime))
-				print('Camera %i collected %i frames at %i fps.' % (n_cam,cnt,fps_count))
+def GetTimeStamp(grabResult, camera):
 
-			# Waits until frame time has been reached to fix frame rate
-			while(time.perf_counter()-timeStart < 1/cam_params["frameRate"]):
-				pass
+	return time.perf_counter()
 
-		except Exception as e:
-			logging.error('Caught exception in grabFrames: {}'.format(e))
-			CloseCamera(cam_params, camera, grabdata)
-			writeQueue.append('STOP')
-			break
+def DisplayImage(cam_params, dispQueue, grabResult):
+	# Downsample image
+	img = grabResult[::cam_params["displayDownsample"],::cam_params["displayDownsample"],:]
 
-def CloseCamera(cam_params, camera, grabdata):
-	n_cam = cam_params["n_cam"]
+	# Send to display queue
+	dispQueue.append(img)
 
-	print('Closing camera {}... Please wait.'.format(n_cam+1))
-	# Close Basler camera after acquisition stops
-	while(True):
-		try:
-			try:
-				SaveMetadata(cam_params,grabdata)
-				time.sleep(1)
-				break
-			except:
-				time.sleep(0.1)
-		except KeyboardInterrupt:
-			break
+def ReleaseFrame(grabResult):
 
-def SaveMetadata(cam_params, grabdata):
-	n_cam = cam_params["n_cam"]
-	full_folder_name = os.path.join(cam_params["videoFolder"], cam_params["cameraName"])
+	del grabResult
 
-	# Save frame numbers and timestamps in numpy array
-	frame_count = grabdata['frameNumber'][-1]
-	time_count = grabdata['timeStamp'][-1]
-	fps_count = int(round(frame_count/time_count))
-	print('Camera {} saved {} frames at {} fps.'.format(n_cam+1, frame_count, fps_count))
-	try:
-		npy_filename = os.path.join(full_folder_name, 'frametimes.npy')
-		x = np.array([grabdata['frameNumber'], grabdata['timeStamp']])
-		np.save(npy_filename,x)
-	except:
-		pass
+def CloseCamera(cam_params, camera):
+	print('Closing {}... Please wait.'.format(cam_params["cameraName"]))
+	# Close camera after acquisition stops
+	del camera
 
-	# Save other recording metadata in csv file
-	meta = cam_params
-	meta['totalFrames'] = grabdata['frameNumber'][-1]
-	meta['totalTime'] = grabdata['timeStamp'][-1]
-	keys = meta.keys()
-	vals = meta.values()
-	
-	csv_filename = os.path.join(full_folder_name, 'metadata.csv')
-	try:
-		with open(csv_filename, 'w', newline='') as f:
-			w = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-			for row in meta.items():
-				w.writerow(row)
-	except:
-		pass
+def CloseSystem(system, device_list):
+	del system
+	del device_list
