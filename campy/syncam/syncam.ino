ab900a5e9f26b42ecd5785bc13397ec1c382c337
Kyle Severson <ksevers2@jhmi.edu>
2021-07-13 21:00:37 -0400

    fixed frame rate errors in Arduino trigger script

--- campy/syncam/syncam.ino
+++ campy/syncam/syncam.ino
@@ -1,10 +1,6 @@
 int DIG_out_pins[] = {2, 4, 7, 8, 12, 13}; // 3, 5, 6, 9, 10, 11 = PWM
 int n_sync = 1;
-long frame_rate = 1; // fps
-long frame_period = 1000 / frame_rate * 1000;
-long pulse_duration = frame_period / 2;
-long frame_start = 0;
-int DIG_level = LOW;
+unsigned long frame_rate, frame_period, pulse_duration; // fps
 
 void setup( void )
 {
@@ -16,42 +12,37 @@ void setup( void )
   }
   
   Serial.begin(115200);
+  Serial.println("Enter your frame rate:");
+  
+  // Wait to receive data from Python or Serial Monitor...
+  while (Serial.available() == 0) {}
 
-  // Wait to receive data from Python...
-  Serial.setTimeout(1000);
-  while ( true )
-  {
-    if (Serial.available() > 0)
-    {
-      frame_rate = Serial.parseInt();
-      Serial.println(frame_rate);
-      frame_period = 1000 / frame_rate * 1000; // microseconds
-      pulse_duration = frame_period / 2;
-      Serial.println(frame_period);
-      Serial.println(pulse_duration);
-      delay(1000);
-      break;
-    }
-  }
-  delay(5000); // Wait n ms for streams and cams to initialize
+  frame_rate = Serial.parseInt();
+  Serial.println(frame_rate);
+  frame_period = 1e6 / frame_rate; // microseconds
+  pulse_duration = frame_period / 2; // ~50% duty cycle
+  Serial.println(frame_period);
+  Serial.println(pulse_duration);
+  delay(2000); // Wait n ms for streams and cams to initialize
 }
 
-void loop( void ) 
-{ 
-  frame_start = micros();
-  for ( byte i = 0; i < n_sync; i++ ) 
-  {
-    digitalWrite(DIG_out_pins[i], HIGH);
+void loop( void ) {
+  noInterrupts();
+  unsigned long frame_start = micros();
+  for ( byte i = 0; i < n_sync; i++ ) {
+    digitalWrite(DIG_out_pins[i], HIGH); 
   }
+  interrupts();
   
-  while ((micros() - frame_start) <= pulse_duration)
-  {} // just wait
-  
-  for ( byte i = 0; i < n_sync; i++ ) 
-  {
-    digitalWrite(DIG_out_pins[i], LOW);
+  while ((micros() - frame_start) < pulse_duration) {
+  } // wait 
+
+  noInterrupts();
+  for ( byte i = 0; i < n_sync; i++ ) {
+    digitalWrite(DIG_out_pins[i], LOW); 
   }
+  interrupts();
   
-  while ((micros() - frame_start) < frame_period)
-  {} // wait for it...
+  while ((micros() - frame_start) < frame_period) {
+  } // wait
 }
