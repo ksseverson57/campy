51ae702551913b4bdc2bab1d515ce807b217cc5a
Kyle Severson <ksevers2@jhmi.edu>
2021-07-18 18:38:41 -0400

    flir testing

--- campy/display/display.py
+++ campy/display/display.py
@@ -5,7 +5,6 @@ import sys
 import time
 import logging
 import numpy as np
-
 import matplotlib as mpl
 mpl.use('Qt5Agg') # disregard qtapp warning...
 import matplotlib.pyplot as plt
@@ -32,7 +31,7 @@ def DisplayFrames(cam_params, dispQueue):
 	n_cam = cam_params['n_cam']
 	
 	if sys.platform == "win32" and cam_params['cameraMake'] == 'basler':
-		# Pylon image window is handled by cameras.basler.cam
+		# Basler Pylon image window is handled by cameras.basler.cam
 		pass
 	else:
 		figure, imageWindow = draw_figure(n_cam+1)
@@ -45,7 +44,7 @@ def DisplayFrames(cam_params, dispQueue):
 						figure.canvas.draw()
 						figure.canvas.flush_events()
 					except Exception as e:
-						logging.error('Caught exception: {}'.format(e))
+						logging.error('Caught exception at display.py DisplayFrames: {}'.format(e))
 				else:
 					time.sleep(0.01)
 			except KeyboardInterrupt:
