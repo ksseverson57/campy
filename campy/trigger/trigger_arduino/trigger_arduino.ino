db8288b1ec22a5a09e0314397c7084ad557eb360
Kyle Severson <ksevers2@jhmi.edu>
2021-07-29 18:15:54 -0400

    updated arduino triggering script

--- campy/trigger/trigger_arduino/trigger_arduino.ino
+++ campy/trigger/trigger_arduino/trigger_arduino.ino
@@ -1,76 +1,127 @@
-// arduino triggering sync
-// inter-frame interval precision is currently ~ 10 us
-// synchronicity precision is ~ 10 ns
+// Camera Sync - TTL triggering with Arduino board
+// Inter-frame interval precision: ~ +/-10 us
+// Inter-frame interval accuracy: ~ +200 us
+// Synchronicity: ~ 10 ns
 
-int DIG_out_pins[] = {2, 4, 7, 8, 12, 13}; // 3, 5, 6, 9, 10, 11 = PWM
-int n_sync = 1;
-volatile unsigned long frame_rate = 0;
-unsigned long frame_period, pulse_duration; // fps
+// User-set parameters
+int DIG_out_pins[] = {2, 4, 7, 8, 12, 13};
+uint32_t baudrate = 115200;
 
-void setup( void )
-{
-  n_sync = int( sizeof(DIG_out_pins) / sizeof(int) );
-  for ( byte i = 0; i < n_sync; i++ ) 
-  {
+// Global variables
+int8_t n_sync;
+float frame_rate_in = 0;
+float frame_rate_out = 0;
+unsigned long time_zero, frame_period, frame_end;
+int32_t frame_count = 0;
+
+
+void setup( void ) {
+  n_sync = int8_t( sizeof(DIG_out_pins) / sizeof(int8_t) );
+  for ( int8_t i = 0; i < n_sync; i++ ) {
     pinMode(DIG_out_pins[i], OUTPUT);
     digitalWrite(DIG_out_pins[i], LOW);
   }
   
-  Serial.begin(115200);
-  Serial.println("Set baudrate to 115200.");
-  Serial.println("Enter your frame rate:");
+  Serial.begin(baudrate);
+  delay(500);
+  Serial.print("Set baudrate to ");
+  Serial.print(baudrate);
+  Serial.println("");
+  Serial.print("Enter your frame rate:");
   
   // Wait to receive data from Python or Serial Monitor...
   while (Serial.available() == 0) {}
 
-  frame_rate = Serial.parseInt();
-  Serial.println(frame_rate);
-  if (frame_rate == 0) { // Avoid divide-by-zero error
-    frame_period = 0xFFFFFFFF;
-    pulse_duration = 0xFFFFFFFF;
-  }
-  else {
-    frame_period = 1e6 / frame_rate; // microseconds
-    pulse_duration = frame_period / 2; // ~50% duty cycle
-  }
-  Serial.println(frame_period);
-  Serial.println(pulse_duration);
-  delay(4000); // Wait n ms for streams and cams to initialize
+  // Parse user input; output frame rate, period
+  frame_rate_out = SetFrameRate();
+  frame_period = SetFramePeriod(frame_rate_out);
+
+  ResetTimer(); // Waits for streams and cams to initialize
 }
 
-void loop( void ) {
-  // Stay on lookout for pyserial com
-  // If message sent, stop triggers
-    if (Serial.available() == 1) {
-      frame_rate = Serial.parseInt();
-      if (frame_rate == 0) { // Timeout (no input) sends '0'
-      }
-      else if (frame_rate == -1) { // encode -1 as stop
-        frame_period = 0xFFFFFFFF;
-        pulse_duration = 0xFFFFFFFF;
-      }
-      else {
-        frame_period = 1e6 / frame_rate; // microseconds
-        pulse_duration = frame_period / 2; // ~50% duty cycle
-      }
-    }
+unsigned long SetFrameRate() {
+  frame_rate_in = Serial.parseFloat();
 
+  // Output frame rate, avoid negative values
+  if (frame_rate_in == 0) {}
+  else if (frame_rate_in < 0) { frame_rate_out = 0; }
+  else { frame_rate_out = frame_rate_in; }
+
+  // Print results (make this a function)
+  Serial.println("");
+  Serial.print("Frame rate set to: ");
+  Serial.print(frame_rate_out);
+  Serial.print(" fps.");
+
+  return frame_rate_out;
+}
+
+unsigned long SetFramePeriod( unsigned long fr ) {
+  // Calculate frame period and pulse duration
+  // Avoid divide-by-zero error
+  if (fr == 0) { frame_period = 0xFFFFFFFF; }
+  else { frame_period = 1e6 / fr; }
+  
+  Serial.println("");
+  Serial.print("Frame period set to: ");
+  Serial.print(frame_period);
+  Serial.print(" microseconds.");
+
+  return frame_period;
+}
+
+void FlushSerialBuffer() {
+  while (Serial.available() != 0) {
+    long floater = Serial.parseFloat();
+  }
+}
+
+void ResetTimer() {
+  delay(4000);
+  FlushSerialBuffer();
+  frame_count = 0;
+  time_zero = micros();
+}
+
+void SetPinsHigh() {
+  // Disable interrupts to synchronize TTLs
   noInterrupts();
-  unsigned long frame_start = micros();
-  for ( byte i = 0; i < n_sync; i++ ) {
+  for ( int8_t i = 0; i < n_sync; i++ ) {
     digitalWrite(DIG_out_pins[i], HIGH); 
   }
   interrupts();
-  
-  while ((micros() - frame_start) < pulse_duration) {
-  } // wait 
+}
 
+void SetPinsLow() {
+  // Disable interrupts to synchronize TTLs
   noInterrupts();
-  for ( byte i = 0; i < n_sync; i++ ) {
+  for ( int8_t i = 0; i < n_sync; i++ ) {
     digitalWrite(DIG_out_pins[i], LOW); 
   }
   interrupts();
+}
+
+void loop( void ) {
+  if (Serial.available() != 0) 
+  {
+    SetPinsLow();
+    frame_rate_out = SetFrameRate();
+    frame_period = SetFramePeriod( frame_rate_out );
+    ResetTimer();
+  }
+
+  if (frame_rate_out > 0) {
+    SetPinsHigh();
+
+    frame_count = frame_count + 1;
+    frame_end = frame_period * frame_count;
+
+    // Wait until end of this pulse period
+    while ((micros() - time_zero) < frame_end - frame_period / 2) {} 
   
-  while ((micros() - frame_start) < frame_period) {
-  } // wait
+    SetPinsLow();
+
+    // Wait until end of this frame period
+    while ((micros() - time_zero) < frame_end) {} 
+  }
 }
