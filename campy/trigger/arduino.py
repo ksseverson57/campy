2cf921f26f0ba682816c1472613154fb9b79a9dd
Kyle Severson <ksevers2@jhmi.edu>
2021-08-30 12:46:54 -0400

    refactored trigger module
    unified arduino and teensy triggering. Campy now accepts digitalPins input in config.

--- campy/trigger/arduino.py
+++ campy/trigger/arduino.py
@@ -6,7 +6,7 @@ Inputs (from config.yaml -> params):
 	serialPort: COM port on PC for Arduino, e.g. 'COM3'
 Currently rely on time delay to allow Arduino to initialize
 TODO: Implement interactive communication link with Python and Arduino
-E.g. Wait for 'Ready' message from Arduino to Python
+E.g. Wait for 'Ready' message from Arduino to Python instead of "dumb" sleep
 """
 
 import serial
@@ -15,11 +15,26 @@ import time, logging
 
 def StartTriggers(systems, params):
 	try:
-		systems["serial"] = serial.Serial(port=params["serialPort"], baudrate=115200, timeout=0.1)
+		# Open serial connection
+		systems["serial"] = serial.Serial(\
+							port=params["serialPort"],
+							baudrate=115200,
+							timeout=0.1)
+
+		# This sleep is important. Wait for Arduino to initialize
 		time.sleep(3)
-		systems["serial"].write(str(params["frameRate"]).encode())
-		print("Arduino on port {} is ready to trigger at {} fps."\
-			.format(params["serialPort"],params["frameRate"]), flush=True)
+
+		# Serialize pin length, IDs, and frame rate to a single string
+		serialList = [len(params["digitalPins"])] + params["digitalPins"] + [params["frameRate"]]
+		systems["serialCommand"] = serialList
+		serialString = ",".join(str(item) for item in serialList)
+
+		# Send command string
+		systems["serial"].write(serialString.encode())
+
+		print("Arduino on port {} is ready to trigger pins {} at {} fps."\
+			.format(params["serialPort"], params["digitalPins"], params["frameRate"]), flush=True)
+
 	except Exception as e:
 		pass
 	return systems
@@ -27,5 +42,17 @@ def StartTriggers(systems, params):
 
 def StopTriggers(systems):
 	print("Closing serial connection...")
-	systems["serial"].write(str(-1).encode())
+
+	# Repeat the same Pyserial command, except encode frame rate as "-1"
+	serialList = systems["serialCommand"]
+	serialList[-1] = -1
+	serialString = ",".join(str(item) for item in serialList)
+
+	# Send command and close connection
+	systems["serial"].write(serialString.encode())
 	systems["serial"].close()
+
+
+def ReceiveReadySignal(systems, params):
+
+	return readySignal
